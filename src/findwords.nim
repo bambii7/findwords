9892b6444a52d3e273bc4767f8266c3426c5d38c
Alexis <alexis@cvofhope.com>
2019-12-18 13:04:30 +1100

    gather and print results

--- src/findwords.nim
+++ src/findwords.nim
@@ -11,6 +11,10 @@ proc matcher(str: string, search: string): bool =
       temp_string.delete(found, found)
   return temp_string.len == 0
 
+proc printResults(results: seq[string]): void =
+  for word in results:
+    echo word
+
 when isMainModule:
   if paramCount() == 2:
     let filename:string = paramStr(1)
@@ -18,11 +22,14 @@ when isMainModule:
 
     var lineCount = 0
     var matchCount = 0
+    var matches: seq[string] = @[]
     for word in lines(filename):
       lineCount.inc
       if(matcher(word.tidy, search)):
+        matches.add(word.tidy)
         matchCount.inc
-        echo word
+
+    printResults(matches)
 
     echo "lines searched: ", $lineCount
     echo "matched: ", $matchCount
