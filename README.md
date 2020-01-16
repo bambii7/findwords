0c7b76975b22a504ba4f5a10d6f7905166ed3d67
Alexis <alexis@cvofhope.com>
2020-01-17 07:41:45 +1100

    export and run test

--- README.md
+++ README.md
@@ -32,3 +32,7 @@ The accuracy is dependant on the ditionary supplied.
 Useful for word search games ;)
 
 There are sample dictionares included but there others are available https://github.com/dwyl/english-words
+
+# Run tests
+
+`nimble test`
--- src/findwords.nim
+++ src/findwords.nim
@@ -1,6 +1,6 @@
 import sequtils, strutils, os, algorithm
 
-proc tidy(s: string): string =
+proc tidy*(s: string): string =
   s.strip.toLowerAscii
 
 proc matcher(str: string, search: string): bool =
@@ -27,6 +27,48 @@ proc paramOrDefault(paramIndex: int, default: int): int =
   except:
     result = default
 
+# proc perm[T](a: openarray[T], n: int, use: var seq[bool]): seq[seq[T]] =
+#   result = newSeq[seq[T]]()
+#   if n <= 0: return
+#   for i in 0 .. a.high:
+#     if not use[i]:
+#       if n == 1:
+#         result.add(@[a[i]])
+#       else:
+#         use[i] = true
+#         for j in perm(a, n - 1, use):
+#           result.add(a[i] & j)
+#         use[i] = false
+
+# proc permutations[T](a: openarray[T], n: int): seq[seq[T]] =
+#   var use = newSeq[bool](a.len)
+#   perm(a, n, use)
+
+# proc toString(str: seq[char]): string =
+#   result = newStringOfCap(len(str))
+#   for ch in str:
+#     add(result, ch)
+
+# # Generating permutation using Heap Algorithm
+# proc heapPermutation(a, size, n):
+
+#     # if size becomes 1 then prints the obtained
+#     # permutation
+#     if (size == 1):
+#         printArr(a, n)
+#         return
+
+#     for i in range(size):
+#         heapPermutation(a,size-1,n);
+
+#         # if size is odd, swap first and last
+#         # element
+#         # else If size is even, swap ith and last element
+#         if size&1:
+#             a[0], a[size-1] = a[size-1],a[0]
+#         else:
+#             a[i], a[size-1] = a[size-1],a[i]
+
 when isMainModule:
   if paramCount() == 2 or paramCount() == 3:
     let filename:string = paramStr(1)
@@ -41,6 +83,11 @@ when isMainModule:
       if(word.len >= minLength and matcher(word.tidy, search)):
         matches.add(word.tidy)
         matchCount.inc
+
+    # let perms = permutations(search, 4)
+    # for perm in perms:
+    #   echo "perm ", perm.toString
+    # echo "total perms ", perms.len
 
     matches.sort(stringCompareHighToLow)
     printResults(matches)
